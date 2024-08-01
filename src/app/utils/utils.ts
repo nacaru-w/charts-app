da7b179a1c2d657f673466ddb4dc92ecbef0e7eb
nacaru-w <ignaciocr92@gmail.com>
2024-08-01 20:44:02 +0200

    Adding utils module

---
+++ src/app/utils/utils.ts
@@ -0,0 +1,23 @@
+export const bodyFont = "system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', 'Noto Sans', 'Liberation Sans', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'";
+export const bodyFontColor = "#212529"
+export const colors = {
+    chartColors: {
+        teal: 'rgba(75, 192, 192, 0.2)',
+        blue: 'rgba(54, 162, 235, 0.2)',
+        purple: 'rgba(153, 102, 255, 0.2)',
+        yellow: 'rgba(255, 205, 86, 0.2)',
+        red: 'rgb(255, 99, 132, 0.2)',
+        gray: 'rgba(128, 128, 128, 0.2)',
+        lightGray: 'rgba(211, 211, 211, 0.2)'
+    },
+    borderColors: {
+        teal: 'rgb(75, 192, 192)',
+        blue: 'rgb(54, 162, 235)',
+        purple: 'rgb(153, 102, 255)',
+        yellow: 'rgba(255, 205, 86)',
+        red: 'rgba(255, 99, 132)',
+        gray: 'rgb(128, 128, 128)',
+        lightGray: 'rgb(211, 211, 211)'
+    }
+
+}
