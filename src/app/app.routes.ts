e0958540c1500119e0018548077b37ed2a5bebd2
nacaru-w <ignaciocr92@gmail.com>
2024-08-01 20:43:55 +0200

    Setting up route paths for every component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
-import { MainComponent } from './components/main/main/main.component';
+import { MainComponent } from './components/main/main.component';
+import { PieComponent } from './components/pie/pie.component';
+import { BarComponent } from './components/bar/bar.component';
+import { LineComponent } from './components/line/line.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/main', pathMatch: 'full' },
-    { path: 'main', component: MainComponent }
+    { path: 'main', component: MainComponent },
+    { path: 'pie', component: PieComponent },
+    { path: 'bar', component: BarComponent },
+    { path: 'line', component: LineComponent },
+    { path: '**', component: MainComponent, pathMatch: 'full' },
 ];
