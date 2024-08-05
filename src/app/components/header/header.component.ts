e2750a15b726c8d42536a1b55ae705ab6b67ca23
nacaru-w <ignaciocr92@gmail.com>
2024-08-05 14:26:43 +0200

    Added explanatory comments

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,12 @@
-import { AfterViewInit, Component } from '@angular/core';
+/**
+ * Componente HeaderComponent
+ * 
+ * Es responsable de renderizar el encabezado de la aplicación, proporcionando 
+ * navegación y selección de idioma. Utiliza @ng-bootstrap para la funcionalidad de navegación 
+ * y menús desplegables, y @ngx-translate/core para la internacionalización.
+ */
+
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgbDropdownModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
@@ -16,7 +24,12 @@ export class HeaderComponent {
   constructor(private translate: TranslateService) {
   }
 
-  switchLanguage(language: Languages) {
+  /**
+  * Se encarga de cambiar el idioma cuando se selecciona en el menú
+  * del encabezado.
+  * @param language: el idioma elegido (en código de idioma);
+  */
+  switchLanguage(language: Languages): void {
     this.translate.setDefaultLang(language);
     this.translate.use(language);
   }
