844308723354ff9e741637d0281f8b2a452328a3
nacaru-w <ignaciocr92@gmail.com>
2024-08-05 14:26:56 +0200

    Reorganised code, added missing functions and explanatory comments

--- src/app/components/bar/bar.component.ts
+++ src/app/components/bar/bar.component.ts
@@ -1,10 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ * Componente BarComponent
+ * 
+ * Renderiza un gráfico de barras que se actualiza en tiempo real con datos simulados 
+ * obtenidos de una API. Utiliza la librería Chart.js para generar el gráfico y
+ * ng2-charts para la integración con Angular. La internacionalización (locale) se gestiona usando
+ * @ngx-translate.
+ * 
+ * Funcionalidades principales:
+ * - Inicializar y renderizar el gráfico de barras.
+ * - Obtener y suscribirse a datos de puntos XY desde un servicio simulado de API.
+ * - Actualizar el gráfico en tiempo real con los datos obtenidos.
+ */
+
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Chart } from 'chart.js/auto';
 import { BaseChartDirective } from 'ng2-charts';
 import { barData, barOptions, labels } from './chart-config/bar-config';
 import { bodyFont, bodyFontColor } from '../../utils/utils';
 import { TranslateService, TranslateModule, LangChangeEvent } from '@ngx-translate/core';
 import { Languages } from '../../models/lang-models';
+import { Subscription } from 'rxjs';
+import { apiService } from '../../services/api.service';
+import { XYPoints } from '../../models/charts-models';
 
 @Component({
   selector: 'app-bar',
@@ -13,30 +30,64 @@ import { Languages } from '../../models/lang-models';
   templateUrl: './bar.component.html',
   styleUrl: './bar.component.scss'
 })
-export class BarComponent implements OnInit {
+export class BarComponent implements OnInit, OnDestroy {
 
-  barChart: any;
+  private subscription: Subscription = new Subscription();
   currentLang: Languages;
+  barChart: any;
 
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private api: apiService) {
     this.currentLang = this.translate.currentLang as Languages;
   }
 
-  ngOnInit(): void {
-    this.buildChart();
+  /**
+   * Se suscribe a los cambios de idioma gestionados con ngx-translate.
+   */
+  private subscribeToLangChange(): void {
     this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
       const language: Languages = event.lang as Languages;
       this.currentLang = language;
       this.updateLabels(this.currentLang);
-    })
+    });
   }
 
-  updateLabels(language: Languages): void {
-    this.barChart.data.labels = labels[language];
-    this.barChart.update()
+  private updateChart(): void {
+    this.barChart?.update()
   }
 
-  buildChart(): void {
+  /**
+  * Actualiza las etiquetas del gráfico según el idioma.
+  * @param language El idioma actual.
+  */
+  private updateLabels(language: Languages): void {
+    if (this.barChart) {
+      this.barChart.data.labels = labels[language];
+      this.updateChart();
+    }
+  }
+
+  /**
+  * Añade puntos al gráfico.
+  * @param pointsObj Los puntos a añadir en forma de objeto.
+  * El objeto contiene la letra y el color como claves
+  */
+  private addPointsToChart(pointsObj: XYPoints): void {
+    // Para poder modificar los datos mediante índice, los datos del array
+    // deben estar dispuestos en el mismo orden que en bar-config.ts
+    const letters: XYPoints["letter"][] = ['x', 'y'];
+    const colors: XYPoints["color"][] = ['teal', 'blue', 'purple'];
+    const letterIndex = letters.indexOf(pointsObj.letter);
+    const colorIndex = colors.indexOf(pointsObj.color);
+    if (this.barChart) {
+      this.barChart.data.datasets[letterIndex].data[colorIndex] += pointsObj.points
+      this.updateChart()
+    }
+  }
+
+  /**
+   * Inicializa el gráfico de barras.
+   */
+  private initializeChart(): void {
     const ctx = <HTMLCanvasElement>document.getElementById('barChart');
 
     Chart.defaults.font.family = bodyFont;
@@ -44,12 +95,31 @@ export class BarComponent implements OnInit {
 
     this.barChart = new Chart(ctx, {
       type: 'bar',
-      data: barData,
-      options: barOptions,
+      data: barData, // Los datos se importan de un archivo específico
+      options: barOptions, // Las opciones de chart.js también
     })
 
-    this.updateLabels(this.currentLang)
+    this.updateLabels(this.currentLang);
+  }
 
+  /**
+  * Se suscribe al endpoint de la API que devuelve los puntos.
+  * Estos puntos se introducirán en el gráfico de forma dinámica.
+  */
+  private subscribeToAPIEndpoint(): void {
+    this.subscription = this.api.getXYPointsFromAPI().subscribe((res: XYPoints) => {
+      this.addPointsToChart(res);
+    })
+  }
+
+  ngOnInit(): void {
+    this.initializeChart();
+    this.subscribeToLangChange();
+    this.subscribeToAPIEndpoint();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/components/bar/chart-config/bar-config.ts
+++ src/app/components/bar/chart-config/bar-config.ts
@@ -3,29 +3,48 @@ import * as en from '../../../../assets/locales/en.json'
 import * as es from '../../../../assets/locales/es.json'
 import * as fr from '../../../../assets/locales/fr.json'
 
+// Se obtienen de la carpeta `locales`, donde se hallan todas las traducciones
 export const labels = {
     en: en.BAR.CHART.LEGEND,
     es: es.BAR.CHART.LEGEND,
     fr: fr.BAR.CHART.LEGEND
 };
 
+// Documentación en https://www.chartjs.org/docs/latest/charts/bar.html 
 export const barData = {
     labels: [],
-    datasets: [{
-        label: 'My First Dataset',
-        data: [27, 12, 31],
-        backgroundColor: [
-            colors.chartColors.teal,
-            colors.chartColors.blue,
-            colors.chartColors.purple
-        ],
-        borderColor: [
-            colors.borderColors.teal,
-            colors.borderColors.blue,
-            colors.borderColors.purple
-        ],
-        borderWidth: 1
-    }]
+    datasets: [
+        {
+            label: 'X',
+            data: [0, 0, 0],
+            backgroundColor: [
+                colors.chartColors.teal,
+                colors.chartColors.blue,
+                colors.chartColors.purple
+            ],
+            borderColor: [
+                colors.borderColors.teal,
+                colors.borderColors.blue,
+                colors.borderColors.purple
+            ],
+            borderWidth: 1
+        },
+        {
+            label: 'Y',
+            data: [0, 0, 0],
+            backgroundColor: [
+                colors.chartColors.darkTeal,
+                colors.chartColors.darkBlue,
+                colors.chartColors.darkPurple
+            ],
+            borderColor: [
+                colors.borderColors.darkTeal,
+                colors.borderColors.darkBlue,
+                colors.borderColors.darkPurple
+            ],
+            borderWidth: 1
+        }
+    ]
 };
 
 export const barOptions = {
@@ -33,7 +52,6 @@ export const barOptions = {
     responsive: true,
     scales: {
         y: {
-            beginAtZero: true,
             ticks: {
                 font: {
                 }
