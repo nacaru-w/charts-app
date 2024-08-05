56dd66a0f1518a44b8f69e266b7d2b9f9d80ae0c
nacaru-w <ignaciocr92@gmail.com>
2024-08-05 14:23:05 +0200

    Adding explanatory comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,13 @@ import { BarComponent } from './components/bar/bar.component';
 import { LineComponent } from './components/line/line.component';
 
 export const routes: Routes = [
+    // La ruta vacía redirige a main
     { path: '', redirectTo: '/main', pathMatch: 'full' },
     { path: 'main', component: MainComponent },
     { path: 'pie', component: PieComponent },
     { path: 'bar', component: BarComponent },
     { path: 'line', component: LineComponent },
+    // La ruta wildcard redirige a main,
+    // idealmente debería redirigir a una página de error 404
     { path: '**', component: MainComponent, pathMatch: 'full' },
 ];
