0cc6da7f53f10f1c65218e9f362d6991ca919a2c
nacaru-w <ignaciocr92@gmail.com>
2024-08-05 14:23:28 +0200

    Added new set of colours

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,14 +1,22 @@
 export const bodyFont = "system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', 'Noto Sans', 'Liberation Sans', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'";
 export const bodyFontColor = "#212529"
+
 export const colors = {
     chartColors: {
         teal: 'rgba(75, 192, 192, 0.2)',
         blue: 'rgba(54, 162, 235, 0.2)',
         purple: 'rgba(153, 102, 255, 0.2)',
         yellow: 'rgba(255, 205, 86, 0.2)',
-        red: 'rgb(255, 99, 132, 0.2)',
+        red: 'rgba(255, 99, 132, 0.2)',
         gray: 'rgba(128, 128, 128, 0.2)',
-        lightGray: 'rgba(211, 211, 211, 0.2)'
+        lightGray: 'rgba(211, 211, 211, 0.2)',
+        darkTeal: 'rgba(50, 128, 128, 0.2)',
+        darkBlue: 'rgba(36, 108, 157, 0.2)',
+        darkPurple: 'rgba(102, 68, 170, 0.2)',
+        darkYellow: 'rgba(170, 137, 58, 0.2)',
+        darkRed: 'rgba(170, 66, 88, 0.2)',
+        darkGray: 'rgba(85, 85, 85, 0.2)',
+        darkLightGray: 'rgba(158, 158, 158, 0.2)'
     },
     borderColors: {
         teal: 'rgb(75, 192, 192)',
@@ -17,7 +25,13 @@ export const colors = {
         yellow: 'rgba(255, 205, 86)',
         red: 'rgba(255, 99, 132)',
         gray: 'rgb(128, 128, 128)',
-        lightGray: 'rgb(211, 211, 211)'
+        lightGray: 'rgb(211, 211, 211)',
+        darkTeal: 'rgb(50, 128, 128)',
+        darkBlue: 'rgb(36, 108, 157)',
+        darkPurple: 'rgb(102, 68, 170)',
+        darkYellow: 'rgba(170, 137, 58)',
+        darkRed: 'rgba(170, 66, 88)',
+        darkGray: 'rgb(85, 85, 85)',
+        darkLightGray: 'rgb(158, 158, 158)'
     }
-
 }
